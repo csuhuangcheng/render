8cd0fa49c4eda6286a29a66912578d13d5e94e7d
Taylor Holberton <taylorcholberton@gmail.com>
2015-04-25 10:20:00 -0700

    fixed print x/y mixup

--- render/debug/image.cpp
+++ render/debug/image.cpp
@@ -8,12 +8,17 @@ namespace debug {
 
 void image::FPrintf(std::ostream& out) const {
 
-	for (uint32_t i = this->RowCount(); i > 0; i--){
+	for (uint32_t y = this->RowCount(); y > 0; y--){
 
-		out << static_cast<int>(i - 1) << ": ";
+		out << static_cast<int>(y - 1) << ": ";
 
-		for (uint32_t j = 0; j < this->RowWidth(); j++){
-			out << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(this->GetValue(i - 1, j)) << " ";
+		for (uint32_t x = 0; x < this->RowWidth(); x++){
+
+			out << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(this->GetValue(x, y - 1));
+
+			if (x + 1 < this->RowWidth()){
+				out << " ";
+			}
 		}
 
 		out << std::endl;
